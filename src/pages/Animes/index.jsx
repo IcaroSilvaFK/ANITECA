fd772c0e467260dac788f31440b2469edc8c5b4b
Icaro Vieira <soprafazermissal@gmail.com>
2022-03-31 17:43:45 -0300

    feat correction css anime page

--- src/pages/Animes/index.jsx
+++ src/pages/Animes/index.jsx
@@ -1,31 +1,44 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
 
+import { BiCommentCheck } from "react-icons/bi";
+
 import { api } from "../../services/axios";
 import { Layout } from "../../layout";
+
+import { Container, Title } from "./styles";
 
 export default function _animePage() {
 	const [page, setPage] = useState(1);
 	const [animes, setAnimes] = useState([]);
-	useEffect(() => {
-		(async () => {
-			const response = await api.get("top/anime", {
-				params: {
-					page,
-				},
-			});
-			if (response.status === 200) {
-				setAnimes(response.data.data);
-			}
-		})();
-	}, [page]);
+	// useEffect(() => {
+	// 	(async () => {
+	// 		const response = await api.get("top/anime", {
+	// 			params: {
+	// 				page,
+	// 			},
+	// 		});
+	// 		if (response.status === 200) {
+	// 			setAnimes(response.data.data);
+	// 		}
+	// 	})();
+	// }, [page]);
 
 	console.log(animes);
 
 	return (
 		<>
 			<Head></Head>
-			<Layout></Layout>
+			<Layout>
+				<Container>
+					<Title>
+						<h1>
+							<BiCommentCheck color="#c53030" />
+							Animes Mais Pesquisados
+						</h1>
+					</Title>
+				</Container>
+			</Layout>
 		</>
 	);
 }
---
+++ src/pages/Animes/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Title = styled.div`
+	display: flex;
+	padding: 20px 50px;
+
+	width: 75%;
+
+	border-bottom: 1px solid #c53030;
+
+	margin: auto;
+
+	h1 {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 10px;
+		font-family: var(--fontMont);
+	}
+`;
